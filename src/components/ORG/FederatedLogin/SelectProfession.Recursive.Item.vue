b3284aef78b7fc122b6b6f5e0b52a18b61077282
Matt Watier <mattwatier@gmail.com>
2022-06-23 11:45:27 -0400

    Professional Type Recursive

---
+++ src/components/ORG/FederatedLogin/SelectProfession.Recursive.Item.vue
@@ -0,0 +1,30 @@
+<template>
+	<ul class="ul_none">
+		<li v-for="(prof, index) in professions" :key="'cata'+index" @click="onSelectCategory(prof)" class="c_black-8 h:c_black-9 h:bg_primary-3 p-x_3 p-y_2" :class="isActive(prof)"><i class="far fa-arrow-circle-right m-r_3"></i>{{ prof["Name"] }}</li>
+	</ul>
+</template>
+
+<script>
+
+
+export default {
+	name: "SelectProfessionItem",
+	props: {
+		professions: { type: Object },
+		currentSelection:{ type:Object }
+	},
+	data() {
+		return {
+
+		}
+	},
+	methods: {
+		isActive(prof){
+			if(prof == this.currentSelection ){return 'bg_primary-3';}
+		},
+		onSelectCategory(prof){
+			this.$emit('onSelect',prof);
+		}
+	},
+};
+</script>
---
+++ src/components/ORG/FederatedLogin/SelectProfession.Recursive.vue
@@ -0,0 +1,43 @@
+<template>
+	<section>
+		<div class="br_2 br_acc br_solid">
+			<span class="block bg_acc p_3 c_white font_bold">I'm a...</span>
+			<professional-item :professions="professions" :currentSelection="currentSelection" @onSelect="onSelect"></professional-item>
+			<!-- <ul class="ul_none">
+				<li v-for="(prof, index) in professions" :key="'cata'+index" @click="onSelectCategory(prof , index)" class="c_black-8 h:c_black-9 h:bg_primary-3 p-x_3 p-y_2" :class="isActive(0,index)"><i class="far fa-arrow-circle-right m-r_3"></i>{{ prof["Name"] }}</li>
+			</ul> -->
+		</div>
+		<div v-if="currentSelection.hasOwnProperty('Subcategories')" class="br_2 br_acc br_solid">
+			<SelectProfession :professions="currentSelection['Subcategories']"></SelectProfession>
+		</div>
+	</section>
+</template>
+
+<script>
+import Btn from "../../subComponents/Btn.vue";
+import Modal from "../../subComponents/Modal.vue";
+import ProfessionalItem from "./SelectProfession.Recursive.Item.vue";
+
+export default {
+	name: "SelectProfession",
+	components: { Btn, Modal, ProfessionalItem },
+	props: {
+		professions: { type: Array },
+		
+	},
+	data() {
+		return {
+			currentSelection: { type: Object }
+		}
+	},
+	computed: {
+
+	},
+	methods: {
+		onSelect(e){
+			this.currentSelection = e
+		}
+
+	},
+};
+</script>
