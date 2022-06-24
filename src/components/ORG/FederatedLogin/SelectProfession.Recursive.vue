6913a81dce574d96850ffb4defe15f3f86838348
Matt Watier <mattwatier@gmail.com>
2022-06-24 09:28:17 -0400

    Cleaned up the Wrapper for Select Profession

--- src/components/ORG/FederatedLogin/SelectProfession.Recursive.vue
+++ src/components/ORG/FederatedLogin/SelectProfession.Recursive.vue
@@ -7,8 +7,8 @@
 				<li v-for="(prof, index) in professions" :key="'cata'+index" @click="onSelectCategory(prof , index)" class="c_black-8 h:c_black-9 h:bg_primary-3 p-x_3 p-y_2" :class="isActive(0,index)"><i class="far fa-arrow-circle-right m-r_3"></i>{{ prof["Name"] }}</li>
 			</ul> -->
 		</div>
-		<div v-if="currentSelection.Subcategories != null && currentSelection.Subcategories.length > 0" class="br_2 br_acc br_solid">
-			<select-profession :professions="currentSelection['Subcategories']" @onChildSelect="onChildSelect"></select-profession>
+		<div v-if="!endOfTree(currentSelection)" class="br_2 br_acc br_solid">
+			<select-profession :professions="currentSelection['Subcategories']" @onChildSelect="onChildSelect" @foundEndOfTree="foundEndOfTree"></select-profession>
 		</div>
 	</section>
 </template>
@@ -55,6 +55,18 @@ export default {
 				}
 			}
 			this.$emit('onChildSelect',{ current:this.currentSelection, child:this.childSelection});
+			if(this.endOfTree(e.current)){
+				this.foundEndOfTree();
+			}
+		},
+		endOfTree(branch){
+			if( Object.keys(branch).length === 0 ){
+				return false;
+			}
+		 	return (branch.Subcategories == null || branch.Subcategories.length == 0 );	
+		},
+		foundEndOfTree(){
+			this.$emit('foundEndOfTree',{ current:this.currentSelection, child:this.childSelection});
 		}
 	},
 };
--- src/components/ORG/FederatedLogin/SelectProfession.Wrapper.vue
+++ src/components/ORG/FederatedLogin/SelectProfession.Wrapper.vue
@@ -3,13 +3,13 @@
 		<label class="label-holder flex font-size_up-1 font_display font_medium p-y_2 lh_1"><span class="text cell flex_shrink">Your Professional Type</span> <span class="required-holder flex_shrink font_n5"><i class="fas fa-asterisk c_warning vertical-align_top"></i></span></label>
 		<div class="flex flex_column-reverse gap-y_3">
 			<Btn class="m-r_4 p_2 p-x_4 self_start" @onClick="modalVisible = true" :type="'button'" :size="'small'"><span>Select</span></Btn>
-			<div v-if="selectedTitles.length" id="prof_breadcrumbs" class="br_2 p-y_2 br_solid flex_auto p-l_4 lh_3 c_black bg_white f:bg_primary br_black-2 br_radius">
-				<span v-for="(string, index) in selectedTitles" :key="'title'+index"><i v-if="index != 0" class="fas fa-angle-right m-x_3 c_primary"></i>{{ string }}</span>
+			<div v-if="path.length" id="prof_breadcrumbs" class="br_2 p-y_2 br_solid flex_auto p-l_4 lh_3 c_black bg_white f:bg_primary br_black-2 br_radius">
+				<span v-for="(value, index) in path" :key="'title'+index"><i v-if="index != 0" class="fas fa-angle-right m-x_3 c_primary"></i>{{ path[index] }}</span>
 			</div>
 		</div>
 		<Modal v-if="modalVisible" @onClose="cancelSelect" class="bg_black-1">
 			<template :class="'font_3 font_bold font_display'" v-slot:header>Select Your Professional Type:</template>
-			<profession-recursive :professions="professions"></profession-recursive>
+			<profession-recursive :professions="professions" @foundEndOfTree="foundEndOfTree"></profession-recursive>
 		</Modal>
 	</div>
 </template>
@@ -24,7 +24,8 @@ export default {
 	components: { Btn, Modal, ProfessionRecursive },
 	props: {
 		professions: { type: Array },
-		currentSelection: { type: Object }
+		currentSelection: { type: Object },
+		childSelection:{type:Object}
 	},
 	data() {
 		return {
@@ -38,50 +39,27 @@ export default {
 			this.getPath(this.professions, this.currentSelection.Code);
 		}
 	},
-	computed: {
-		selectedTitles(){
-			const titles = this.path.map( x => x.Name);
-			return titles;
-		}
-	},
 	methods: {
-		getPath: function(array, code){
-			for(let item of array){
-				if(item.hasOwnProperty('Code') && item.Code === code){
-					this.path.unshift({ ...item, index:array.indexOf(item) });
-					return item;
-				}
-				if(item.Subcategories) {
-					const child = this.getPath(item.Subcategories, code);
+		foundEndOfTree(e){
+			this.currentSelection = e.current;
+			this.childSelection = e.child;
+			this.modalVisible = false;
+			this.path = [];
+			this.getPath(e.child);
+		},
+		getPath: function(object){
+			this.path.push(object.Name);
+			if(object.Subcategories) {
+					const child = this.getPath(object.Subcategories);
 					if(child) {
 						this.path.unshift({ ...item, index:array.indexOf(item) });
 						return child;
 					}
         		}
-    		}
-		},
-		isActive(cat,index){
-			if((this.path.length > cat) && index == this.path[cat].index) {
-				return 'bg_primary-3';
-			}
-		},
-		onSelectCategory(item, index){
-			this.path = [];
-			if(!item.hasOwnProperty('Code')){
-				this.path.unshift({ ...item, index:index })
-			} else {
-				this.getPath(this.professions, item.Code);
-			}
 
-			if(!item.hasOwnProperty('Subcategories')){
-				this.$emit('onSelect',item);
-				this.modalVisible = false;
-			}
 		},
 		cancelSelect(){
 			this.modalVisible = false;
-			this.path = [];
-			this.getPath(this.professions, this.currentSelection.Code);
 		}
 	},
 };
